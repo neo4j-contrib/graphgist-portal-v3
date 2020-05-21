f0350d5f8b3ea1cdaa9755d870ed9ed904aaec54
Alisson Patricio <eu@alisson.net>
2020-05-21 20:27:54 -0300

    GGP-286: Graphgist title and author search

--- api/src/index.js
+++ api/src/index.js
@@ -18,8 +18,20 @@ const app = express();
  * https://grandstack.io/docs/neo4j-graphql-js-api.html#makeaugmentedschemaoptions-graphqlschema
  */
 
+const S3_BUCKET_NAME = process.env.S3_BUCKET_NAME;
+
 export const schema = makeAugmentedSchema({
   typeDefs,
+  resolvers: {
+    Image: {
+      source_url: (obj, args, context, info) => {
+        const {uuid, source_file_name, source_updated_at} = obj.source_url.properties;
+        const size = "medium";
+        const partitions = uuid.match(/.{9}/g)[0].match(/.{1,3}/g).join("/");
+        return `https://${S3_BUCKET_NAME}.s3.amazonaws.com/graph_starter/images/sources/${partitions}/${size}/${source_file_name}?${source_updated_at}`;
+      }
+    }
+  },
   config: {
     mutation: false
   }
