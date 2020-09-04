87baa964ada74e0d99cee860edfd87f19cebe115
Alisson Patricio <eu@alisson.net>
2020-09-04 12:08:07 -0300

    create graphgist candidate before editing

--- api/src/index.js
+++ api/src/index.js
@@ -1,7 +1,7 @@
 import { ApolloServer, AuthenticationError } from "apollo-server-express";
 import express from "express";
 import neo4j from 'neo4j-driver';
-import { makeAugmentedSchema } from "neo4j-graphql-js";
+import { makeAugmentedSchema, neo4jgraphql } from "neo4j-graphql-js";
 import { v4 as uuidv4 } from 'uuid';
 import Asciidoctor from 'asciidoctor';
 import { getGraphGistByUUID } from './graphgists';
@@ -189,7 +189,81 @@ export const schema = makeAugmentedSchema({
           console.error(error)
         }
         return null;
-      }
+      },
+      getGraphGistCandidate: async (obj, args, context, info) => {
+				const session = context.driver.session();
+        const txc = session.beginTransaction();
+        const { uuid: graphGistUUID } = args;
+
+        // var gc = {
+        //   render_id: uuid,
+        //   summary: '',
+        //   cached: null,
+        //   author
+        // };
+
+        try {
+          const graphGist = await getGraphGistByUUID(txc, graphGistUUID);
+          if (!graphGist) {
+            throw new Error("GraphGist not found");
+          }
+
+          const result = await txc.run(`
+            MATCH (g:GraphGist {uuid: $uuid})<-[:IS_VERSION]-(c:GraphGistCandidate)
+            RETURN c
+          `, { uuid: graphGistUUID });
+					
+					let candidateUUID;
+          if (result.records.length > 0) {
+            await txc.commit();
+						candidateUUID	= result.records[0].get('c').properties.uuid;
+          } else {
+            candidateUUID = uuidv4();
+						const createResult = await txc.run(`
+							MATCH (g:GraphGist {uuid: $uuid})
+              MERGE (c:GraphGistCandidate)-[r:IS_VERSION]->(g)
+							SET c = g
+              SET c.uuid = $candidateUUID
+              WITH c, g
+
+              MATCH (g)<-[r:WROTE]-(p) 
+              WITH collect(p) as endNodes, c, g
+              FOREACH(pp in endNodes | CREATE (c)<-[:WROTE]-(pp))
+              WITH c, g
+
+              MATCH (g)-[r:FOR_CHALLENGE]->(p) 
+              WITH collect(p) as endNodes, c, g
+              FOREACH(pp in endNodes | CREATE (c)-[:FOR_CHALLENGE]->(pp))
+              WITH c, g
+
+              MATCH (g)-[r:FOR_INDUSTRY]->(p) 
+              WITH collect(p) as endNodes, c, g
+              FOREACH(pp in endNodes | CREATE (c)-[:FOR_INDUSTRY]->(pp))
+              WITH c, g
+
+              MATCH (g)-[r:FOR_USE_CASE]->(p) 
+              WITH collect(p) as endNodes, c, g
+              FOREACH(pp in endNodes | CREATE (c)-[:FOR_USE_CASE]->(pp))
+              WITH c, g
+
+							RETURN c
+						`, {
+							uuid: graphGistUUID,
+              candidateUUID
+						});
+            await txc.commit();
+          }
+
+					return neo4jgraphql(obj, { uuid: candidateUUID }, context, info);
+        } catch (error) {
+          console.error(error)
+          await txc.rollback()
+        } finally {
+          await session.close()
+        }
+
+				return null;
+      },
     },
     Image: {
       source_url: (obj, args, context, info) => {
