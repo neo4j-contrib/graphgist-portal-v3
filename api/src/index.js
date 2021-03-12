aafddae8739c21197b286ffca195ad6b3de11e2f
Alisson Patricio <eu@alisson.net>
2021-03-12 13:35:33 -0300

    fix timeout issue with submitting to approval

--- api/src/index.js
+++ api/src/index.js
@@ -64,7 +64,10 @@ app.use(Sentry.Handlers.tracingHandler());
 
 app.use(express.json({ limit: "50mb" }));
 app.use(express.urlencoded({ limit: "50mb", extended: true }));
-app.use(cors());
+app.use(cors({
+  origin: true,
+  credentials: true
+}));
 app.set("view engine", "ejs");
 
 /*
