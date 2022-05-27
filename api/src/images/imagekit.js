1b561bc893d600035a8abf9174e232f7d8e1eea3
Guillaume Grossetie <ggrossetie@yuzutech.fr>
2022-05-27 13:54:16 +0200

    resolves #64 defer ImageKit initialization

--- api/src/images/imagekit.js
+++ api/src/images/imagekit.js
@@ -3,19 +3,44 @@ const ImageKit = require("imagekit");
 var fs = require('fs');
 dotenv.config();
 
-const imagekit = new ImageKit({
-  publicKey : process.env.IMAGEKIT_PUBLIC_KEY,
-  privateKey : process.env.IMAGEKIT_PRIVATE_KEY,
-  urlEndpoint: process.env.IMAGEKIT_URL_ENDPOINT
-});
+const ImageService = (function () {
+  class ImageServiceClass {
+    constructor() {
+      this.imagekit = new ImageKit({
+        publicKey: process.env.IMAGEKIT_PUBLIC_KEY,
+        privateKey: process.env.IMAGEKIT_PRIVATE_KEY,
+        urlEndpoint: process.env.IMAGEKIT_URL_ENDPOINT
+      });
+    }
+
+    upload(file, uuid) {
+      return this.imagekit.upload({
+        file, //required
+        fileName : uuid, //required
+      })
+    }
+    delete(fileId) {
+      return this.imagekit.deleteFile(fileId)
+    }
+  }
+
+  let instance;
+  return {
+    getInstance: function () {
+      if (instance == null) {
+        instance = new ImageServiceClass();
+        // Hide the constructor so the returned object can't be new'd...
+        instance.constructor = null;
+      }
+      return instance;
+    }
+  };
+})();
 
 export async function uploadImage(file, uuid) {
-  return imagekit.upload({
-      file, //required
-      fileName : uuid, //required
-  })
+  return ImageService.getInstance().upload(file, uuid)
 }
 
 export async function deleteImage(fileId) {
-  return imagekit.deleteFile(fileId)
+  return ImageService.getInstance().delete(fileId)
 }
